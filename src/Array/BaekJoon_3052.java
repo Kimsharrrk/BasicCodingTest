30d97068caa8afee35e7d1c4c94dc7be6855dfe1
Kimsharrrk <108858402+Kimsharrrk@users.noreply.github.com>
2025-07-31 18:58:38 +0900

    7월 31일

--- src/Array/BaekJoon_3052.java
+++ src/Array/BaekJoon_3052.java
@@ -3,25 +3,27 @@ package Array;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class BaekJoon_3052 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        int []num = new int[10];
+        int[] num = new int[10];
 
-        for (int i=0 ; 10 > i; i++){
-             num[i] = Integer.parseInt(bf.readLine());
+        for (int i = 0; i < 10; i++) {
+            num[i] = Integer.parseInt(bf.readLine()) % 42;
         }
 
+        Arrays.sort(num);
 
-
-        for (int i= 0;10>i; i++){
-            num[i] = num[i] % 42;
-            System.out.println(num[i]);
+        int count = 1; // 처음 수는 무조건 1번 등장
+        for (int i = 0; i < 9; i++) {
+            if (num[i] != num[i + 1]) {
+                count++;
+            }
         }
 
-        //bolean 써서 if 로 비교하면 될듯.. 잠시 눕자...
-
+        System.out.println(count);
     }
 }
