ce26c594f6d8f3467dc2fdb708ad735b391a0548
Kimsharrrk <108858402+Kimsharrrk@users.noreply.github.com>
2025-08-17 00:08:23 +0900

    8월 7일

--- src/Graph_Traversal/BaekJoon_2606_바이러스.java
+++ src/Graph_Traversal/BaekJoon_2606_바이러스.java
@@ -1,66 +1,66 @@
-package Graph_Traversal;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
-public class BaekJoon_2606_바이러스 {
-
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    static boolean Visited[];
-    static int count = 0;
-
-    public static void dfs(int node) {
-        Visited[node] = true;
-
-        for (int next : graph.get(node)) {
-            if (!Visited[next]) {
-                count++;
-                dfs(next); //재귀호출
-            }
-
-
-        }
-    }
-
-        public static void main (String[]args) throws IOException {
-            BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-
-            //1. 컴퓨터수 노드의수
-            int computer = Integer.parseInt(bf.readLine());
-
-
-            //2. 컴퓨터의 순서쌍 간선의 수
-
-            for (int i = 0; i <= computer; i++) {
-                graph.add(new ArrayList<>());
-            }
-            Visited = new boolean[computer + 1]; // 방문 배열 초기화
-
-
-            int computerLine = Integer.parseInt(bf.readLine());
-
-            for (int i = 0 ; computerLine > i ; i++){
-                StringTokenizer st = new StringTokenizer(bf.readLine());
-
-                 int A = Integer.parseInt(st.nextToken());
-                 int B = Integer.parseInt(st.nextToken());
-
-                graph.get(A).add(B);
-                graph.get(B).add(A);
-
-            }
-
-            dfs(1);
-
-            System.out.println(count);
-
-
-
-        }
-
-
-}
+//package Graph_Traversal;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.ArrayList;
+//import java.util.StringTokenizer;
+//
+//public class BaekJoon_2606_바이러스 {
+//
+//    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+//    static boolean Visited[];
+//    static int count = 0;
+//
+//    public static void dfs(int node) {
+//        Visited[node] = true;
+//
+//        for (int next : graph.get(node)) {
+//            if (!Visited[next]) {
+//                count++;
+//                dfs(next); //재귀호출
+//            }
+//
+//
+//        }
+//    }
+//
+//        public static void main (String[]args) throws IOException {
+//            BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//
+//
+//            //1. 컴퓨터수 노드의수
+//            int computer = Integer.parseInt(bf.readLine());
+//
+//
+//            //2. 컴퓨터의 순서쌍 간선의 수
+//
+//            for (int i = 0; i <= computer; i++) {
+//                graph.add(new ArrayList<>());
+//            }
+//            Visited = new boolean[computer + 1]; // 방문 배열 초기화
+//
+//
+//            int computerLine = Integer.parseInt(bf.readLine());
+//
+//            for (int i = 0 ; computerLine > i ; i++){
+//                StringTokenizer st = new StringTokenizer(bf.readLine());
+//
+//                 int A = Integer.parseInt(st.nextToken());
+//                 int B = Integer.parseInt(st.nextToken());
+//
+//                graph.get(A).add(B);
+//                graph.get(B).add(A);
+//
+//            }
+//
+//            dfs(1);
+//
+//            System.out.println(count);
+//
+//
+//
+//        }
+//
+//
+//}
--- src/String/BaekJoon_11718_그대로출력하기.java
+++ src/String/BaekJoon_11718_그대로출력하기.java
@@ -1,5 +1,27 @@
 package String;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BaekJoon_11718_그대로출력하기 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String A = "";
+
+
+        while((A = bf.readLine()) != null) {
+            System.out.println(A);
+        }
+
+
+
+
+
+
+    }
 
 }
