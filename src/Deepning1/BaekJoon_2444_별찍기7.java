954f0c3d6909f0ac53b20830db972407f4b82550
Kimsharrrk <108858402+Kimsharrrk@users.noreply.github.com>
2025-08-20 23:49:35 +0900

    8월 7일

--- src/Deepning1/BaekJoon_2444_별찍기7.java
+++ src/Deepning1/BaekJoon_2444_별찍기7.java
@@ -1,37 +1,24 @@
-//package Deepning1;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//
-//public class BaekJoon_2444_별찍기7 {
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-//
-//        int num = Integer.parseInt(bf.readLine());
-//
-//        for (int i = 1; num >= i; i++) {
-//            for (int j = 1; num - i >= j; j++) {
-//                System.out.print(" ".repeat(j));
-//
-//            }
-//
-//            for (int j = 1; j <= 2*i-1; j++) {
-//                System.out.println("*".repeat(j));
-//            }
-//        }
-//
-////        int j = num-1;
-////        for (int i = 1; num >= i ; i++){
-////
-////            System.out.print(" ".repeat(j));
-////            System.out.println("*".repeat(i));
-////
-////            j--;
-////
-////
-////
-////
-////        }
-//    }
-//}
+package Deepning1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon_2444_별찍기7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bf.readLine());
+
+        // 위쪽 삼각형
+        for (int i = 1; i <= num; i++) {
+            System.out.print(" ".repeat(num - i));
+            System.out.println("*".repeat(2 * i - 1));
+        }
+
+        // 아래쪽 삼각형
+        for (int i = num - 1; i >= 1; i--) {
+            System.out.print(" ".repeat(num - i));
+            System.out.println("*".repeat(2 * i - 1));
+        }
+    }
+}
